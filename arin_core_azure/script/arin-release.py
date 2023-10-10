afa56d151905dc575d250caf1f284d72b8342b35
JaapOosterbroek <jaap@secureailabs.com>
2023-10-10 10:51:14 +0300

    increment

--- arin_core_azure/script/arin-release.py
+++ arin_core_azure/script/arin-release.py
@@ -8,6 +8,20 @@ def usage() -> None:
     print("Usage: python arin-release.py [patch, minor, major], [code, docker, all]")
 
 
+def bump_version(version: str, release_type: str) -> str:
+    version_parts = version.split(".")
+    if release_type == "patch":
+        version_parts[2] = str(int(version_parts[2]) + 1)
+    elif release_type == "minor":
+        version_parts[1] = str(int(version_parts[1]) + 1)
+        version_parts[2] = "0"
+    elif release_type == "major":
+        version_parts[0] = str(int(version_parts[0]) + 1)
+        version_parts[1] = "0"
+        version_parts[2] = "0"
+    return ".".join(version_parts)
+
+
 def main() -> None:
     # check arguments and set default values
     if len(sys.argv) < 3:
@@ -37,8 +51,18 @@ def main() -> None:
     # get latest tagged version #TODO figure out how to deal with patches
     result = subprocess.run("git tag", capture_output=True, shell=True)
     tagged_versions = result.stdout.decode("utf-8").strip().split("\n")
-    latest_tagged_version = tagged_versions[-1]
-    print(f"latest tagged version: {latest_tagged_version}")
+    if len(tagged_versions) == 0:
+        target_version = current_version
+        print(f"No tagged versions found. Releasing as {target_version}")
+    else:
+        latest_tagged_version = tagged_versions[-1]
+        print(f"Latest tagged version: {latest_tagged_version}")
+        if latest_tagged_version != current_version:
+            target_version = current_version
+            print(f"Latest tagged version does not match current version. Releasing as {target_version}")
+        else:
+            target_version = bump_version(current_version, release_type)
+            print(f"Latest tagged version match current version. Bumping and releasing as as {target_version}")
 
     # ARIN_PYPI_REPOSITORY_URL = os.environ["ARIN_PYPI_REPOSITORY_URL"]
     # ARIN_PYPI_USERNAME = os.environ["ARIN_PYPI_USERNAME"]
